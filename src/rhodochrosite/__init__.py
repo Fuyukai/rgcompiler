34b3d6148195058234da4463c9ee20f71854bc65
Lura Skye <fuyukaii@proton.me>
2025-02-01 04:44:00 +0000

    add py.typed and writer exports

--- src/rhodochrosite/__init__.py
+++ src/rhodochrosite/__init__.py
@@ -1,6 +1,13 @@
 from rhodochrosite.reader import (
     MarshalReader as MarshalReader,
-    RubyMarshalValue as RubyMarshalValue,
     read_object as read_object,
 )
-from rhodochrosite.ruby import RubySymbol as RubySymbol
+from rhodochrosite.ruby import (
+    AnyRubyObject as AnyRubyObject,
+    CustomMarshal as CustomMarshal,
+    GenericRubyObject as GenericRubyObject,
+    RubyMarshalValue as RubyMarshalValue,
+    RubySymbol as RubySymbol,
+    UnknownUserDefined as UnknownUserDefined,
+)
+from rhodochrosite.writer import MarshalWriter as MarshalWriter
