5b557bfb02538c8ac3aba5bbf7761a7fdf1f8bac
Lura Skye <fuyukaii@proton.me>
2025-02-02 19:47:07 +0000

    add dialogue event commands

--- src/rgcompiler/ruby/rpg/event_commands.py
+++ src/rgcompiler/ruby/rpg/event_commands.py
@@ -119,8 +119,64 @@ class UnknownEventCommand(RubyBaseEventCommand):
         return super().unstructure(converter)
 
 
+@attr.define(kw_only=True)
+class ShowDialogueCommand(RubyBaseEventCommand):
+    """
+    An event command that shows dialogue to the player.
+    """
+
+    text: str = attr.field()
+
+    @classmethod
+    @override
+    def from_raw_event_command(cls, cmd: RawEventCommand) -> ShowDialogueCommand:
+        return ShowDialogueCommand(text=cast(str, cmd.parameters[0]))
+
+    @override
+    def get_raw_event_command(self) -> RawEventCommand:
+        return RawEventCommand(
+            code=101,
+            parameters=[self.text],
+        )
+
+    @override
+    def unstructure(self, converter: Converter) -> dict[str, Any]:
+        return {
+            "command": "ShowDialogueCommand",
+            "text": self.text,
+        }
+
+
+@attr.define(kw_only=True)
+class ContinueDialogueCommand(ShowDialogueCommand):
+    """
+    An event command that continues dialogue.
+    """
+
+    @classmethod
+    @override
+    def from_raw_event_command(cls, cmd: RawEventCommand) -> ContinueDialogueCommand:
+        return ContinueDialogueCommand(text=cast(str, cmd.parameters[0]))
+
+    @override
+    def get_raw_event_command(self) -> RawEventCommand:
+        return RawEventCommand(
+            code=401,
+            parameters=[self.text],
+        )
+
+    @override
+    def unstructure(self, converter: Converter) -> dict[str, Any]:
+        return {
+            "command": "ContinueDialogueCommand",
+            "text": self.text,
+        }
+
+
 COMMAND_MAPPING: dict[int, type[RubyBaseEventCommand]] = {
     0: EmptyEventCommand,
+    101: ShowDialogueCommand,
+    401: ContinueDialogueCommand,
 }
 
 
