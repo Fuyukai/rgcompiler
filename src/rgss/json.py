75d6588155be32d620cc045574127a6cb111c74e
Lura Skye <fuyukaii@proton.me>
2025-02-04 11:48:59 +0000

    omit defaults when unstructuring a few types
    this cleans up the json dumps

--- src/rgss/json.py
+++ src/rgss/json.py
@@ -1,9 +1,11 @@
 from functools import partial
 from typing import Any
 
-from cattr import Converter
+from cattrs import Converter
+from cattrs.gen import make_dict_unstructure_fn
 
 from rgss.rpg.commands.base import RubyBaseCommand
+from rgss.rpg.event import RubyEventGraphic, RubyEventPageCondition
 from rgss.types import RgssDirection
 from rhodochrosite.ruby import GenericRubyUserObject, RubySymbol
 
@@ -32,6 +34,15 @@ def make_converter() -> Converter:
 
     converter = Converter()
     converter.register_unstructure_hook(RgssDirection, lambda it: it.name)
+
+    converter.register_unstructure_hook(
+        RubyEventPageCondition,
+        make_dict_unstructure_fn(RubyEventPageCondition, converter, _cattrs_omit_if_default=True),
+    )
+    converter.register_unstructure_hook(
+        RubyEventGraphic,
+        make_dict_unstructure_fn(RubyEventGraphic, converter, _cattrs_omit_if_default=True),
+    )
 
     converter.register_unstructure_hook(RubySymbol, partial(unstructure_symbol, converter))
     converter.register_unstructure_hook(RubyBaseCommand, lambda it: it.unstructure(converter))
